8005ba5129f03e344120652043eae69158e5e146
Syntax-Programmer <anand6308anand@gmail.com>
2025-12-03 01:36:27 +0530

    Made a code timing and benchmarking utility.

---
+++ Utils/Timer.c
@@ -0,0 +1,80 @@
+#include "Defs.h"
+#define _POSIX_C_SOURCE 199309L // Enable clock_gettime on POSIX systems
+#include "Logger.h"
+#include "Timer.h"
+
+/* ----  TIME MEASUREMENT  ---- */
+
+static PRP_TimeMeasure GetTimeNs();
+
+#if defined(_WIN32) || defined(_WIN64)
+#include <windows.h>
+
+static PRP_TimeMeasure GetTimeNs() {
+    static LARGE_INTEGER freq;
+    static PRP_I32 initialized = 0;
+    LARGE_INTEGER counter;
+    if (!initialized) {
+        QueryPerformanceFrequency(&freq);
+        initialized = 1;
+    }
+    QueryPerformanceCounter(&counter);
+    return (PRP_TimeMeasure)counter.QuadPart * 1e9 / freq.QuadPart;
+}
+
+#else
+#include <time.h>
+
+static PRP_TimeMeasure GetTimeNs() {
+    struct timespec ts;
+    clock_gettime(CLOCK_MONOTONIC, &ts);
+    return ts.tv_sec * 1e9 + ts.tv_nsec;
+}
+#endif
+
+static const PRP_f64 TIME_UNIT_SCALE[] = {1, 1e3, 1e6, 1e9};
+
+PRP_FN_API PRP_TimeMeasure PRP_FN_CALL PRP_TimerGetTime(PRP_TimeUnit unit) {
+    if (unit < 0 || unit > PRP_TIME_S) {
+        unit = PRP_TIME_NS; // fallback
+    }
+
+    return GetTimeNs() / TIME_UNIT_SCALE[unit];
+}
+
+/* ----  TIMER  ---- */
+
+#define TIMER_VALIDITY_CHECK(timer, ret)                                       \
+    do {                                                                       \
+        if (!timer) {                                                          \
+            PRP_LOG_FN_INV_ARG_ERROR(timer);                                   \
+            return ret;                                                        \
+        }                                                                      \
+    } while (0)
+
+PRP_FN_API PRP_FnCode PRP_FN_CALL PRP_TimerStart(PRP_Timer *timer) {
+    TIMER_VALIDITY_CHECK(timer, PRP_FN_INV_ARG_ERROR);
+
+    timer->start = PRP_TimerGet(timer->unit);
+
+    return PRP_FN_SUCCESS;
+}
+
+PRP_FN_API PRP_TimeMeasure PRP_FN_CALL PRP_TimerElapsed(PRP_Timer *timer) {
+    TIMER_VALIDITY_CHECK(timer, PRP_INVALID_TIME_MEASURE);
+
+    return PRP_TimerGetTime(timer->unit) - timer->start;
+}
+
+PRP_FN_API PRP_FnCode PRP_FN_CALL PRP_TimerChangeUnit(PRP_Timer *timer,
+                                                      PRP_TimeUnit unit) {
+    TIMER_VALIDITY_CHECK(timer, PRP_FN_INV_ARG_ERROR);
+    if (unit < 0 || unit > PRP_TIME_S) {
+        unit = PRP_TIME_NS; // fallback
+    }
+
+    timer->unit = unit;
+    timer->start = PRP_TimerGetTime(timer->unit);
+
+    return PRP_FN_SUCCESS;
+}
---
+++ Utils/Timer.h
@@ -0,0 +1,81 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "Defs.h"
+
+/* ----  TIME MEASUREMENT  ---- */
+
+/**
+ * The units of time that the timer supports.
+ *
+ * PRP_TIME_NS: Nanoseconds
+ * PRP_TIME_US: Microseconds
+ * PRP_TIME_MS: Milliseconds
+ * PRP_TIME_S: Seconds
+ */
+typedef enum { PRP_TIME_NS, PRP_TIME_US, PRP_TIME_MS, PRP_TIME_S } PRP_TimeUnit;
+
+// The actual time measure that we will get in float.
+typedef PRP_f64 PRP_TimeMeasure;
+#define PRP_INVALID_TIME_MEASURE (-1)
+
+/**
+ * Gets the current time in the specified unit.
+ *
+ * @param unit: The unit to get the result into.
+ *
+ * @return The time in the given unit, with PRP_TIME_NS as a fallback if unit is
+ * invalid.
+ */
+PRP_FN_API PRP_TimeMeasure PRP_FN_CALL PRP_TimerGet(PRP_TimeUnit unit);
+
+/* ----  TIMER  ---- */
+
+/**
+ * Keeps track of the start and the current unit of the timer.
+ */
+typedef struct {
+    PRP_TimeMeasure start;
+    PRP_TimeUnit unit;
+} PRP_Timer;
+
+#define PRP_TIMER(unit) ((PRP_Timer){.unit = (unit)})
+
+/**
+ * Starts the timer by setting the current time. To set current time to the
+ * timer simply recall this same function on the timer and it will update the
+ * timer to the current time.
+ *
+ * @param timer: The timer to start.
+ *
+ * @return PRP_FN_INV_ARG_ERROR if the timer is invalid, otherwise
+ * PRP_FN_SUCCESS.
+ */
+PRP_FN_API PRP_FnCode PRP_FN_CALL PRP_TimerStart(PRP_Timer *timer);
+/**
+ * Gets the total elapsed time from the timer start till the call time.
+ *
+ * @param timer: The timer to get elapsed time for.
+ *
+ * @return PRP_INVALID_TIME_MEASURE if the timer is invalid, otherwise the
+ * elapsed time in the timer's units.
+ */
+PRP_FN_API PRP_TimeMeasure PRP_FN_CALL PRP_TimerElapsed(PRP_Timer *timer);
+/**
+ * Changes the unit of the timer and updates the currently set time safely.
+ *
+ * @param timer: The timer to change the unit for.
+ * @param unit: The new unit of the timer to set.
+ *
+ * @return PRP_INVALID_TIME_MEASURE if the timer is invalid, otherwise
+ * PRP_FN_SUCCESS.
+ */
+PRP_FN_API PRP_FnCode PRP_FN_CALL PRP_TimerChangeUnit(PRP_Timer *timer,
+                                                      PRP_TimeUnit unit);
+
+#ifdef __cplusplus
+}
+#endif
