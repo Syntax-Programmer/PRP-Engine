5f9a859e78847e26f79f792b60c9a44e52753292
Syntax-Programmer <anand6308anand@gmail.com>
2026-05-10 22:28:37 +0530

    Added utility functions to the euler angle

--- Math/EulerAngle.h
+++ Math/EulerAngle.h
@@ -4,7 +4,8 @@
 extern "C" {
 #endif
 
-#include "Float.h"
+#include "Consts.h"
+#include "Transcendental.h"
 
 typedef enum {
     MATH_EULER_ANGLE_ORDER_XYZ,
@@ -20,6 +21,44 @@ typedef struct {
     DT_f32 y_rad;
     DT_f32 z_rad;
 } MATH_EulerAngle;
+
+static inline MATH_EulerAngle MATH_EulerAngleCreateZero(DT_void) {
+    return (MATH_EulerAngle){0};
+}
+
+static inline MATH_EulerAngle
+MATH_EulerAngleCreateFromRad(DT_f32 x_rad, DT_f32 y_rad, DT_f32 z_rad) {
+    return (MATH_EulerAngle){
+        .x_rad = x_rad,
+        .y_rad = y_rad,
+        .z_rad = z_rad,
+    };
+}
+
+static inline MATH_EulerAngle
+MATH_EulerAngleCreateFromDeg(DT_f32 x_deg, DT_f32 y_deg, DT_f32 z_deg) {
+    return (MATH_EulerAngle){
+        .x_rad = MATH_DEG_TO_RAD_F32 * x_deg,
+        .y_rad = MATH_DEG_TO_RAD_F32 * y_deg,
+        .z_rad = MATH_DEG_TO_RAD_F32 * z_deg,
+    };
+}
+
+static inline MATH_EulerAngle MATH_EulerAngleWrapToPi(MATH_EulerAngle angle) {
+    return (MATH_EulerAngle){
+        .x_rad = MATH_WrapAngleToPiF32(angle.x_rad),
+        .y_rad = MATH_WrapAngleToPiF32(angle.y_rad),
+        .z_rad = MATH_WrapAngleToPiF32(angle.z_rad),
+    };
+}
+
+static inline MATH_EulerAngle MATH_EulerAngleWrapToTau(MATH_EulerAngle angle) {
+    return (MATH_EulerAngle){
+        .x_rad = MATH_WrapAngleToTauF32(angle.x_rad),
+        .y_rad = MATH_WrapAngleToTauF32(angle.y_rad),
+        .z_rad = MATH_WrapAngleToTauF32(angle.z_rad),
+    };
+}
 
 #ifdef __cplusplus
 }
