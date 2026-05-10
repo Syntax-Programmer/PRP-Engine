91ed61765f7415aca8bac2e88ebeb3a813cfafd9
Syntax-Programmer <anand6308anand@gmail.com>
2026-05-10 23:40:42 +0530

    Forgot to add this :)

--- Math/Math.h
+++ Math/Math.h
@@ -13,6 +13,15 @@ extern "C" {
 #include "Vector/Vec3.h"
 #include "Vector/Vec4.h"
 
+#include "Matrix/Mat2/Mat2-Affine.h"
+#include "Matrix/Mat2/Mat2.h"
+
+#include "Matrix/Mat3/Mat3-Affine.h"
+#include "Matrix/Mat3/Mat3.h"
+
+#include "Matrix/Mat4/Mat4-Affine.h"
+#include "Matrix/Mat4/Mat4.h"
+
 #ifdef __cplusplus
 }
 #endif
