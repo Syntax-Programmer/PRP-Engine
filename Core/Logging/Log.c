aafe77d710d634d3502b47300a414e8a770380ef
Syntax-Programmer <anand6308anand@gmail.com>
2026-08-09 15:33:20 +0530

    Added logging module, still the thread safety is handled by posix thread library till the threading subsystem matures.

--- Core/Logging/Log.c
+++ Core/Logging/Log.c
@@ -1,17 +1,146 @@
 #include "Log.h"
+#include <pthread.h>
 #include <stdarg.h>
 
-PRP_API PRP_Size PRP_CALL PRP_LogGetFinalFormatSize(const PRP_Char8 *pMsg, ...);
-PRP_API void PRP_CALL PRP_LogFormat(PRP_Char8 *pDest, PRP_Size dest_size,
-                                    const PRP_Char8 *pMsg, ...);
-PRP_API void PRP_CALL PRP_LogFormatF(FILE *pDest, const PRP_Char8 *pMsg, ...);
+/**
+ * This is a placeholder tech so that the logger can work before the true
+ * threading library can be completed.
+ */
+static pthread_mutex_t log_mutex = PTHREAD_MUTEX_INITIALIZER;
 
-PRP_API void PRP_CALL PRP_LogWrite(const PRP_Char8 *pMsg, PRP_Size len);
-PRP_API void PRP_CALL PRP_LogWriteLn(const PRP_Char8 *pMsg, PRP_Size len);
-PRP_API void PRP_CALL PRP_LogWriteChar(PRP_Char8 c);
+PRP_API PRP_I32 PRP_CALL PRP_LogFormat(PRP_Char8 *pDest, PRP_Size dest_size,
+                                       const PRP_Char8 *pMsg, ...) {
+    if (!pDest || !pMsg || !dest_size) {
+        return -1;
+    }
 
-PRP_API void PRP_CALL PRP_LogWriteF(const PRP_Char8 *pMsg, PRP_Size len,
-                                    FILE *pFile);
-PRP_API void PRP_CALL PRP_LogWriteLnF(const PRP_Char8 *pMsg, PRP_Size len,
-                                      FILE *pFile);
-PRP_API void PRP_CALL PRP_LogWriteCharF(PRP_Char8 c, FILE *pFile);
+    va_list args;
+    va_start(args, pMsg);
+    PRP_I32 result = vsnprintf(pDest, dest_size, pMsg, args);
+    va_end(args);
+
+    return result;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogFormatF(FILE *pDest, const PRP_Char8 *pMsg,
+                                           ...) {
+    if (!pDest || !pMsg) {
+        return PRP_ERR_INV_ARG;
+    }
+
+    va_list args;
+    va_start(args, pMsg);
+    if (vfprintf(pDest, pMsg, args) < 0) {
+        return PRP_ERR_IO;
+    }
+    va_end(args);
+
+    return PRP_OK;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogWrite(const PRP_Char8 *pMsg, PRP_Size len) {
+    if (!pMsg || !len) {
+        return PRP_ERR_INV_ARG;
+    }
+
+    pthread_mutex_lock(&log_mutex);
+
+    PRP_Result code = PRP_OK;
+    if (fwrite(pMsg, 1, len, PRP_LOG_DEFAULT_LOG_FILE) != len) {
+        code = PRP_ERR_IO;
+    }
+
+    pthread_mutex_unlock(&log_mutex);
+
+    return code;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogWriteLn(const PRP_Char8 *pMsg,
+                                           PRP_Size len) {
+    if (!pMsg || !len) {
+        return PRP_ERR_INV_ARG;
+    }
+
+    pthread_mutex_lock(&log_mutex);
+
+    PRP_Result code = PRP_OK;
+    if (fwrite(pMsg, 1, len, PRP_LOG_DEFAULT_LOG_FILE) != len) {
+        code = PRP_ERR_IO;
+    }
+    if (fwrite("\n", 1, 1, PRP_LOG_DEFAULT_LOG_FILE) != 1) {
+        code = PRP_ERR_IO;
+    }
+
+    pthread_mutex_unlock(&log_mutex);
+
+    return code;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogWriteChar(PRP_Char8 c) {
+    pthread_mutex_lock(&log_mutex);
+
+    PRP_Result code = PRP_OK;
+    if (fwrite(&c, 1, 1, PRP_LOG_DEFAULT_LOG_FILE) != 1) {
+        code = PRP_ERR_IO;
+    }
+
+    pthread_mutex_unlock(&log_mutex);
+
+    return code;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogWriteF(const PRP_Char8 *pMsg, PRP_Size len,
+                                          FILE *pFile) {
+    if (!pMsg || !len || !pFile) {
+        return PRP_ERR_INV_ARG;
+    }
+
+    pthread_mutex_lock(&log_mutex);
+
+    PRP_Result code = PRP_OK;
+    if (fwrite(pMsg, 1, len, pFile) != len) {
+        code = PRP_ERR_IO;
+    }
+
+    pthread_mutex_unlock(&log_mutex);
+
+    return code;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogWriteLnF(const PRP_Char8 *pMsg, PRP_Size len,
+                                            FILE *pFile) {
+    if (!pMsg || !len || !pFile) {
+        return PRP_ERR_INV_ARG;
+    }
+
+    pthread_mutex_lock(&log_mutex);
+
+    PRP_Result code = PRP_OK;
+    if (fwrite(pMsg, 1, len, pFile) != len) {
+        code = PRP_ERR_IO;
+    }
+    if (fwrite("\n", 1, 1, pFile) != 1) {
+        code = PRP_ERR_IO;
+    }
+
+    pthread_mutex_unlock(&log_mutex);
+
+    return code;
+}
+
+PRP_API PRP_Result PRP_CALL PRP_LogWriteCharF(PRP_Char8 c, FILE *pFile) {
+    if (!pFile) {
+        return PRP_ERR_INV_ARG;
+    }
+
+    pthread_mutex_lock(&log_mutex);
+
+    PRP_Result code = PRP_OK;
+    if (fwrite(&c, 1, 1, pFile) != 1) {
+        code = PRP_ERR_IO;
+    }
+
+    pthread_mutex_unlock(&log_mutex);
+
+    return code;
+}
--- Core/Logging/Log.h
+++ Core/Logging/Log.h
@@ -9,20 +9,107 @@ extern "C" {
 
 #define PRP_LOG_DEFAULT_LOG_FILE (stdout)
 
-PRP_API PRP_Size PRP_CALL PRP_LogGetFinalFormatSize(const PRP_Char8 *pMsg, ...);
-PRP_API void PRP_CALL PRP_LogFormat(PRP_Char8 *pDest, PRP_Size dest_size,
-                                    const PRP_Char8 *pMsg, ...);
-PRP_API void PRP_CALL PRP_LogFormatF(FILE *pDest, const PRP_Char8 *pMsg, ...);
+/**
+ * Formats the given string into pDest.
+ *
+ * @oaram pDest     Output pointer of the formatted string.
+ * @param dest_size The size of the pDest buffer allocated.
+ * @param pMsg      The message to format.
+ *
+ * @return Negative number if arguments are invalid.
+ * @return Negative number if formatting fails.
+ * @return The total size of the formatted string on success.
+ *
+ * @note Behaviour:
+ * - If dest_size is less than the required size, the message will be truncated.
+ *
+ */
+PRP_API PRP_I32 PRP_CALL PRP_LogFormat(PRP_Char8 *pDest, PRP_Size dest_size,
+                                       const PRP_Char8 *pMsg, ...);
+/**
+ * Formats the given string into the pDest file.
+ *
+ * @param pDest Output file pointer.
+ * @param pMsg  The message to format.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if the formatting into the file failed.
+ * @return PRP_ERR_INV_ARG if arguments are invalid.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogFormatF(FILE *pDest, const PRP_Char8 *pMsg,
+                                           ...);
 
-PRP_API void PRP_CALL PRP_LogWrite(const PRP_Char8 *pMsg, PRP_Size len);
-PRP_API void PRP_CALL PRP_LogWriteLn(const PRP_Char8 *pMsg, PRP_Size len);
-PRP_API void PRP_CALL PRP_LogWriteChar(PRP_Char8 c);
+/**
+ * Writes the given string message to the default log dest.
+ *
+ * @param pMsg The message to log.
+ * @param len  The explicit len of the message.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if write operation failed.
+ * @return PRP_ERR_INV_ARG if arguments are invalid.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogWrite(const PRP_Char8 *pMsg, PRP_Size len);
+/**
+ * Writes the given string message plus a new line character to the default log
+ * dest.
+ *
+ * @param pMsg The message to log.
+ * @param len  The explicit len of the message.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if write operation failed.
+ * @return PRP_ERR_INV_ARG if arguments are invalid.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogWriteLn(const PRP_Char8 *pMsg, PRP_Size len);
+/**
+ * Writes the given character to the default log dest.
+ *
+ * @param c The character to log.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if write operation failed.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogWriteChar(PRP_Char8 c);
 
-PRP_API void PRP_CALL PRP_LogWriteF(const PRP_Char8 *pMsg, PRP_Size len,
-                                    FILE *pFile);
-PRP_API void PRP_CALL PRP_LogWriteLnF(const PRP_Char8 *pMsg, PRP_Size len,
-                                      FILE *pFile);
-PRP_API void PRP_CALL PRP_LogWriteCharF(PRP_Char8 c, FILE *pFile);
+/**
+ * Writes the given string message to the file dest.
+ *
+ * @param pMsg  The message to log.
+ * @param len   The explicit len of the message.
+ * @param pFile The destination file to log into.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if write operation failed.
+ * @return PRP_ERR_INV_ARG if arguments are invalid.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogWriteF(const PRP_Char8 *pMsg, PRP_Size len,
+                                          FILE *pFile);
+/**
+ * Writes the given string message plus a new line character to the default log
+ * dest.
+ *
+ * @param pMsg  The message to log.
+ * @param len   The explicit len of the message.
+ * @param pFile The destination file to log into.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if write operation failed.
+ * @return PRP_ERR_INV_ARG if arguments are invalid.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogWriteLnF(const PRP_Char8 *pMsg, PRP_Size len,
+                                            FILE *pFile);
+/**
+ * Writes the given character to the default log dest.
+ *
+ * @param c     The character to log.
+ * @param pFile The destination file to log into.
+ *
+ * @return PRP_OK on success.
+ * @return PRP_ERR_IO if write operation failed.
+ * @return PRP_ERR_INV_ARG if arguments are invalid.
+ */
+PRP_API PRP_Result PRP_CALL PRP_LogWriteCharF(PRP_Char8 c, FILE *pFile);
 
 #ifdef __cplusplus
 }
